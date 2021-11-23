af44088a6cbb445ffbb75d77a8afe05ffede8208
MengshiChen <moussechen0205@gmail.com>
2021-11-22 22:06:48 -0800

    fixed bugs

--- main.js
+++ main.js
@@ -1,96 +1,179 @@
-import * as THREE from '../src/three.js';
+import * as THREE from 'https://cdn.jsdelivr.net/npm/three@0.118/build/three.module.js';
+import {
+  GLTFLoader
+} from 'https://cdn.jsdelivr.net/npm/three@0.118.1/examples/jsm/loaders/GLTFLoader.js';
+import {
+  OrbitControls
+} from 'https://cdn.jsdelivr.net/npm/three@0.118/examples/jsm/controls/OrbitControls.js';
+// import * as THREE from "./src/three.module.js";
+//
+// // Import add-ons for glTF models, orbit controls, and font loader
+// import{
+//   OrbitControls
+// } from 'https://cdn.jsdelivr.net/npm/three@0.118/examples/jsm/controls/OrbitControls.js';
+// import {
+// 	GLTFLoader
+// } from "./src/GLTFLoader.js";
 
-			import Stats from '../src/GLTFLoader.js';
-			import OrbitControls from '../src/OrbitControls.js';
-      import {
-        RoomEnvironment
-      } from 'https://cdn.jsdelivr.net/npm/three@0.118/examples/jsm/environments/RoomEnvironment.js';
+let container, scene, camera, renderer, mesh, mesh2, mixer, controls, clock;
 
-      // Set height and width to the size of our browser window
-      let width = window.innerWidth,
-          height = window.innerHeight;
+let ticker = 0;
 
-      // RENDERER
-      // Create a new renderer and attach it to our canvas element
-      // Set antialiasing to true and alpha (transparency) buffer to true
-      // Set the size equal to our window.
-      const renderer = new THREE.WebGLRenderer({
-          canvas: myCanvas,
-          antialias: true,
-          alpha: true
-      });
-      renderer.setSize(width, height);
+// Call init and animate functions (defined below)
+init();
+animate();
 
-      // SCENE
-      // Create a new Three scene
-      const scene = new THREE.Scene();
+function init() {
 
-      // CAMERA
-      // Create a camera, zoom it out from the model a bit, and add it to the scene.
-      // Params are vertical field of view, aspect ratio, near plane, and far plane.
-      const camera = new THREE.PerspectiveCamera(45, width / height, 10, 2000);
-      camera.position.set(0,2,100);
-      scene.add(camera);
+	//Identify div in HTML to place scene
+	container = document.getElementById("space");
 
-      // LIGHT
-      // Create a source of white light at half intensity and add to scene
-      const color = 0xFFFFFF;
-      const intensity = 0.8;
-      const light = new THREE.DirectionalLight(color, intensity);
-      light.position.set(100, 50, 100);
-      light.target.position.set(0, 0, 0);
-      scene.add(light);
-      scene.add(light.target);
+	//Crate clock for animation
+	clock = new THREE.Clock();
 
-      // LOADING 3D MODEL
-      // Assigning logoMesh to the gltf scene, then adding it to the scene
-      // We will use the scope of logoMesh to be able to rotate it in animate
-      const loader = new THREE.GLTFLoader();
-      loader.load('./assets/logo.glb', handle_load);
-      let logoMesh;
-      function handle_load(gltf) {
-          logoMesh = gltf.scene;
-          scene.add(logoMesh);
-      }
+	//Create scene
+	scene = new THREE.Scene();
+	camera = new THREE.PerspectiveCamera(
+		50,
+		window.innerWidth / window.innerHeight,
+		0.1,
+		1000
+	);
+	renderer = new THREE.WebGLRenderer();
+	renderer.setClearColor(0xdfdfdf);
+	renderer.setPixelRatio(window.devicePixelRatio);
+	renderer.setSize(window.innerWidth, window.innerHeight);
+	// Add scene to gltf.html
+	container.appendChild(renderer.domElement);
 
-      // RESIZE LISTENER
-      // This is listening for any window resizing and will reset the aspect ratio of the render
-      // and updates camera aspect ratio as well so logo is always in center of browser window.
-      window.addEventListener('resize', function() {
-          width = window.innerWidth,
-          height = window.innerHeight;
-          renderer.setSize(width, height);
-          camera.aspect = width / height;
-          camera.updateProjectionMatrix();
-      });
+	// Material to be added to preanimated model
+	var newMaterial = new THREE.MeshStandardMaterial({
+		color: 0x2E5939
+	});
 
-      // Use OrbitControls to control camera view via mouse
-      const controls = new THREE.OrbitControls(camera, renderer.domElement);
+	// Load preanimated model, add material, and add it to the scene
+	const loader = new GLTFLoader().load(
+		"./assets/logo.glb",
+		function(gltf) {
+			gltf.scene.traverse(function(child) {
+				if (child.isMesh) {
+					// child.material = newMaterial;
+				}
+			});
+			// set position and scale
+			mesh = gltf.scene;
+			mesh.position.set(4, 0, -6);
+			mesh.rotation.set(0, 0, 0);
+			mesh.scale.set(1, 1, 1);
+			// Add model to scene
+			scene.add(mesh);
+			//Check for and play animation frames
+			mixer = new THREE.AnimationMixer(mesh);
+			gltf.animations.forEach((clip) => {
+				mixer.clipAction(clip).play();
+			});
 
-      function animate () {
-          // Render the scene with camera information
-          renderer.render(scene, camera);
+		},
+		undefined,
+		function(error) {
+			console.error(error);
+		}
+	);
 
-          // Rotate our cube
-          // arrayCube.map(cube => {
-          //     cube.rotation.x += Math.random() / 80;
-          //     cube.rotation.y += Math.random() / 80;
-          //     cube.position.x += (Math.random() / 50 - Math.random() / 50);
-          //     cube.position.y += (Math.random() / 50 - Math.random() / 50);
-          // });
+	// Add Orbit Controls
+	controls = new OrbitControls(camera, renderer.domElement);
+	controls.minDistance = 3;
+	controls.maxDistance = 35;
+	controls.target.set(0, 0, -0.2);
 
-          // Rotate our logo
-          if (logoMesh) {
-              logoMesh.rotation.x += 0.02
-              logoMesh.rotation.y += 0.01
-              logoMesh.rotation.z += 0.01
-          }
+	// Position our camera so we can see the shape
+	camera.position.z = 20;
 
-          controls.update();
+	// Add a directional light to the scene
+	const directionalLight = new THREE.DirectionalLight(0xffffff, 0.9);
+	scene.add(directionalLight);
 
-          // Get new information and rerender at 60fps
-          requestAnimationFrame(animate);
-      }
+	// Add an ambient light to the scene
+	const ambientLight = new THREE.AmbientLight(0xffffff, 0.9);
+	scene.add(ambientLight);
 
-      // Start the animation loop
-      animate();
+
+}
+
+// Define animate loop
+function animate () {
+		// Render the scene with camera information
+		renderer.render(scene, camera);
+
+		// Rotate our cube
+		// arrayCube.map(cube => {
+		//     cube.rotation.x += Math.random() / 80;
+		//     cube.rotation.y += Math.random() / 80;
+		//     cube.position.x += (Math.random() / 50 - Math.random() / 50);
+		//     cube.position.y += (Math.random() / 50 - Math.random() / 50);
+		// });
+
+		// Rotate our logo
+		if (mesh) {
+				mesh.rotation.x += 0.02
+				mesh.rotation.y += 0.01
+				mesh.rotation.z += 0.01
+		}
+
+		controls.update();
+
+		// Get new information and rerender at 60fps
+		requestAnimationFrame(animate);
+}
+
+// Start the animation loop
+animate();
+
+// // Define the render loop
+// function render() {
+//   renderer.render(scene, camera);
+//   // manualAnimation();
+// }
+
+// Manual Looping animation for mesh2
+// function manualAnimation() {
+//   if (ticker == 0) {
+//     if (mesh2.scale.x < 2) {
+//       mesh2.scale.x += 0.00341;
+//     }
+//     if (mesh2.scale.x >= 2 && mesh2.scale.y < 2) {
+//       mesh2.scale.y += 0.00341;
+//     }
+//     if (mesh2.scale.x >= 2 && mesh2.scale.y >= 2 && mesh2.scale.z < 2) {
+//       mesh2.scale.z += 0.00341;
+//     }
+//     if (mesh2.scale.x >= 2 && mesh2.scale.y >= 2 && mesh2.scale.z >= 2) {
+//       ticker = 1;
+//     }
+//   }
+//   if (ticker == 1) {
+//     if (mesh2.scale.x >= 2 && mesh2.scale.y >= 2 && mesh2.scale.z > 1) {
+//       mesh2.scale.z -= 0.00341;
+//     }
+//     if (mesh2.scale.x >= 2 && mesh2.scale.y > 1 && mesh2.scale.z <= 1) {
+//       mesh2.scale.y -= 0.00341;
+//     }
+//     if (mesh2.scale.x > 1 && mesh2.scale.y <= 1 && mesh2.scale.z <= 1) {
+//       mesh2.scale.x -= 0.00341;
+//     }
+//     if (mesh2.scale.x <= 1 && mesh2.scale.y <= 1 && mesh2.scale.z <= 1) {
+//       ticker = 0;
+//     }
+//   }
+// }
+
+// Respond to Window Resizing
+window.addEventListener("resize", onWindowResize);
+
+// Window resizing function
+function onWindowResize() {
+	camera.aspect = window.innerWidth / window.innerHeight;
+	camera.updateProjectionMatrix();
+	renderer.setSize(window.innerWidth / 2, window.innerHeight / 2);
+	render();
+}
